bada98ae607d04380e19aa4dbca0f2dc867e7023
Souleimane-ifqirne <ifqirne.souleimane@gmail.com>
2023-09-19 15:54:18 +0200

    feat(index): ui/ux, template

---
+++ src/script.js
@@ -0,0 +1,5 @@
+var sendTo = document.getElementById("sign-up")
+
+sendTo.addEventListener("click", function () {
+    window.location.href = "./lazone.html"
+});
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '98c73f81d86884dbcd64d4ec87c12018a75274e3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '98c73f81d86884dbcd64d4ec87c12018a75274e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'altorouter/altorouter' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '09d9d946c546bae6d22a7654cdb3b825ffda54b4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../altorouter/altorouter',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
