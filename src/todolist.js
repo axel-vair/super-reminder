41648e5b83ced4481b53fbcad7c4e06622b1abae
Axel Vair <contact@axelvair.fr>
2023-09-22 11:47:07 +0200

    :sparkles: display todo

--- src/todolist.js
+++ src/todolist.js
@@ -4,12 +4,27 @@ async function requestTodoList() {
     let response = await fetch(request);
     let responseData = await response.json();
     if (responseData) {
-        let todoList = document.getElementById('todo-list');
-        responseData.forEach((element) => {
-            let li = document.createElement('li');
-            li.innerHTML = element.title;
-            console.log(li)
-        });
+        let todoList = document.getElementById('list-todo');
+        Object.values(responseData).forEach(val =>
+            {
+                    let li = document.createElement('li');
+                    let template = `<div class="todo-item">
+                                        <div class="todo-item-title">   
+                                            <span class="todo-item-title">${val.title}</span>
+                                            <span class="todo-item-task">${val.task}</span>
+                                            <span class="todo-item-date">${val.createdAt}</span>
+                                        </div>
+                                        <div class="todo-item-actions">
+                                            <button class="todo-item-actions-button todo-item-actions-button--done" data-id="${val.id}">Fait</button>
+                                            <button class="todo-item-actions-button todo-item-actions-button--delete" data-id="${val.id}">Supprimer</button>
+                                        </div>
+                                    </div>`;
+                    li.innerHTML = template;
+                    todoList.appendChild(li);
+
+
+            });
+
 
 
     }else{
