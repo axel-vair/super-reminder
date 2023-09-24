7a2f3be8c393801a9b827c44d3d4b01c49a94919
Axel Vair <contact@axelvair.fr>
2023-09-24 16:56:50 +0200

    :sparkles: add move div and updated date

--- controller/todolist-update.php
+++ controller/todolist-update.php
@@ -4,4 +4,4 @@ session_start();
 require "../vendor/autoload.php";
 use Reminder\Models\Todo;
 $update = new Todo();
-@$update->updateTodo($_GET['update']);
+@$update->updateTodo($_GET['update'], date('Y-m-d H:i:s'));
